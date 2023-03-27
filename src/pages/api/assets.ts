514b19ff3a69c70249cfb1bf8406c64301b0d7e7
Jacob Hansen <jacob.a.hansen2@gmail.com>
2023-03-26 23:15:39 -0500

    adds recursive file url retriever for assets

--- src/pages/api/assets.ts
+++ src/pages/api/assets.ts
@@ -1,11 +1,26 @@
 import { promises as fs } from "fs";
 import path from "path";
 
+async function traverseDirectory(dir, basePath = "") {
+  const entries = await fs.readdir(dir, { withFileTypes: true });
+  const filePaths = await Promise.all(
+    entries.map(async (entry) => {
+      const entryPath = path.join(dir, entry.name);
+      const relativePath = path.join(basePath, entry.name);
+      if (entry.isDirectory()) {
+        return traverseDirectory(entryPath, relativePath);
+      } else {
+        return relativePath;
+      }
+    })
+  );
+  return Array.prototype.concat(...filePaths);
+}
+
 export default async function handler(req, res) {
   try {
     const publicDir = path.join(process.cwd(), "public");
-    const files = await fs.readdir(publicDir);
-    const assets = files.map((file) => `/public/${file}`);
+    const assets = await traverseDirectory(publicDir);
     res.status(200).json({ assets });
   } catch (err) {
     res.status(500).json({ error: err.message });
